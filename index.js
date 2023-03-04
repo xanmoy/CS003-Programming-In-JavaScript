53a6c8befbe9674e20bcdaa40fb967d0fa3a528d
Tanmoy Gangopadhyay <tanmoygangulyofficial@outlook.com>
2023-03-04 17:38:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+2+3
+alert(2+3);
+typeof(23);
+typeof("Tanmoy");
+typeof(true);
+var myName = "Tanmoy";
+prompt(myName);
+var myName = "Tanmoy";
+var yourname = prompt("My name is" + myName +", welcome to my website" + yourname + "!" );
+var gamelevel = 1;
+gamelevel=2;
+gamelevel=3;
+alert("yor laval is currently: " + gamelevel)
---
+++ main.js
@@ -0,0 +1,7 @@
+alert("Hello");
+alert("World");
+2+3
+alert(2+3);
+typeof(23);
+typeof("Tanmoy");
+typeof(true);
